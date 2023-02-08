6b4d67df2d003b89f77611cc55aefa8ddbe1dd2d
Aaron Purnomo Murniadi <aaron.murniadi@ninjavan.co>
2023-02-08 13:35:51 +0700

    optimize imports

--- src/test/java/co/nvqa/common/core/client/RouteMonitoringClientTest.java
+++ src/test/java/co/nvqa/common/core/client/RouteMonitoringClientTest.java
@@ -1,22 +1,19 @@
 package co.nvqa.common.core.client;
 
-import co.nvqa.common.utils.NvRetry;
-import co.nvqa.common.utils.DateUtil;
 import co.nvqa.common.core.model.RouteMonitoringResponse;
-import co.nvqa.common.utils.NvTestRuntimeException;
-import com.mysql.cj.log.Log;
+import co.nvqa.common.utils.DateUtil;
+import co.nvqa.common.utils.NvRetry;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 import uk.org.webcompere.systemstubs.resource.PropertySource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ExtendWith(SystemStubsExtension.class)
 public class RouteMonitoringClientTest implements NvRetry {
