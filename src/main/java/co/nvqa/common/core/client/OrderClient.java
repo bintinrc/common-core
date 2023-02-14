6aabd857317e3af636fd5aec66798cfe5cb04115
Binti Cahayati <binti.cahayati@ninjavan.co>
2023-02-14 15:39:26 +0700

    wip until OrderClient

--- src/main/java/co/nvqa/common/core/client/OrderClient.java
+++ src/main/java/co/nvqa/common/core/client/OrderClient.java
@@ -2,6 +2,7 @@ package co.nvqa.common.core.client;
 
 import co.nvqa.common.client.SimpleApiClient;
 import co.nvqa.common.constants.HttpConstants;
+import co.nvqa.common.core.model.order.BulkForceSuccessOrderRequest;
 import co.nvqa.common.core.model.order.Order;
 import co.nvqa.common.core.model.order.Order.BillingZone;
 import co.nvqa.common.core.model.order.Order.Cod;
@@ -9,10 +10,13 @@ import co.nvqa.common.core.model.order.Order.PackageContent;
 import co.nvqa.common.core.model.order.Order.PricingInfo;
 import co.nvqa.common.core.model.order.Order.ShipperRefMetadata;
 import co.nvqa.common.core.model.order.Order.Transaction;
+import co.nvqa.common.core.model.order.OrderTagsRequest;
 import co.nvqa.common.core.model.order.RescheduleOrderRequest;
 import co.nvqa.common.core.model.order.RescheduleOrderResponse;
+import co.nvqa.common.core.model.order.RtsOrderRequest;
 import co.nvqa.common.core.model.order.SearchOrderRequest;
 import co.nvqa.common.core.model.order.SearchOrderResponse;
+import co.nvqa.common.core.model.order.SearchOrderTagResponse;
 import co.nvqa.common.utils.NvTestHttpException;
 import co.nvqa.common.utils.StandardTestConstants;
 import co.nvqa.commonauth.utils.TokenUtils;
@@ -20,11 +24,24 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.inject.Singleton;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class OrderClient extends SimpleApiClient {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(OrderClient.class);
 
   public OrderClient() {
     super(StandardTestConstants.API_BASE_URL, TokenUtils.getOperatorAuthToken(),
@@ -58,6 +75,30 @@ public class OrderClient extends SimpleApiClient {
     }
   }
 
+  public List<Order> searchOrders(SearchOrderRequest searchOrderRequest) {
+    Response r = searchOrderAndGetRawResponse(searchOrderRequest);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+
+    SearchOrderResponse result = fromJson(r.body().asString(), SearchOrderResponse.class);
+    return result.getOrders();
+  }
+
+  public Order searchOrderByTrackingIdUsingSubstring(String trackingIdSubString) {
+    SearchOrderRequest searchOrderRequest = new SearchOrderRequest(trackingIdSubString);
+    searchOrderRequest.setExact(Boolean.FALSE);
+    return searchOrder(searchOrderRequest);
+  }
+
+  /**
+   * extra method especially made to handle some legacy scenarios
+   */
+  public Response searchOrderByTrackingIdAndGetRawResponse(String trackingId) {
+    SearchOrderRequest searchOrderRequest = new SearchOrderRequest(trackingId);
+    return searchOrderAndGetRawResponse(searchOrderRequest);
+  }
+
   public Response searchOrderAndGetRawResponse(SearchOrderRequest searchOrderRequest) {
     String url = "core/2.1/orders/search";
     String json = toJson(searchOrderRequest);
@@ -85,6 +126,12 @@ public class OrderClient extends SimpleApiClient {
     return doGet("Core - Get Order", spec, url);
   }
 
+  public void forceSuccess(long orderId) {
+    boolean codCollected = false;
+
+    forceSuccess(orderId, codCollected);
+  }
+
   public void forceSuccess(long orderId, boolean codCollected) {
     Response r = forceSuccessAsRawResponse(orderId, codCollected);
     if (r.statusCode() != HttpConstants.RESPONSE_204_NO_CONTENT) {
@@ -100,6 +147,94 @@ public class OrderClient extends SimpleApiClient {
         .param("reason", "FORCE_SUCCESS_QA_AUTO");
 
     return doPut("Core - Force Success Order", spec, url);
+  }
+
+  public void bulkForceSuccess(List<BulkForceSuccessOrderRequest> request) {
+    String url = "core/orders/forcesuccess-bulk";
+    String json = toJsonSnakeCase(request);
+    RequestSpecification spec = createAuthenticatedRequest()
+        .body(json);
+    Response r = doPost("Operator Portal - Bulk Force Success Order", spec, url);
+    r.then().contentType(ContentType.JSON);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+  }
+
+  public void forceSuccess(Set<Long> orderIds) {
+    if (Optional.ofNullable(orderIds).isPresent()) {
+      orderIds.forEach(this::forceSuccess);
+    }
+  }
+
+  public void batchRecalculate(String eventType, String... trackingIds) {
+    String url = "core/2.0/orders/batch-recalculate";
+    if (Optional.ofNullable(trackingIds).isPresent()) {
+      Map<String, Object> request = new HashMap<>();
+      request.put("tracking_ids", Arrays.stream(trackingIds).collect(Collectors.toList()));
+      request.put("event", eventType);
+
+      Response r = doPut("Core - Batch Recalculate", createAuthenticatedRequest().body(request),
+          url);
+      if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+        throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+      }
+    } else {
+      LOGGER.warn("Could not do 'batch recalculate' because the Tracking ID(s) is empty.");
+    }
+  }
+
+  public void updateGranularStatusOrder(Long orderId, String granularStatus) {
+    String url = "core/2.0/orders/{orderId}/update-status";
+    String body = String
+        .format("{\"granular_status\": \"%s\",\"reason\": \"update order granular status\"}",
+            granularStatus);
+    RequestSpecification spec = createAuthenticatedRequest()
+        .pathParam("orderId", orderId)
+        .body(body);
+
+    Response r = doPut("Core - Update Order Granular Status", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_204_NO_CONTENT) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+  }
+
+  public Response editDeliveryVerificationRequiredAndGetRawResponse(String trackingId,
+      String value) {
+    String url = "core/orders/delivery-mechanism";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .header("Accept", ContentType.JSON)
+        .header("Content-Type", ContentType.JSON)
+        .body(f("{\"tracking_id\":\"%s\",\"delivery_authorization_method\":\"%s\"}", trackingId,
+            value));
+
+    return doPost("Core - Update Delivery Verification Required", spec, url);
+  }
+
+  public Response validateDeliveryVerificationAndGetRawResponse(String trackingId) {
+    String url = "core/orders/delivery-mechanism/validation";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .header("Accept", ContentType.JSON)
+        .header("Content-Type", ContentType.JSON)
+        .queryParam("tracking_id", trackingId);
+
+    return doGet("Core - Validate Order ATL", spec, url);
+  }
+
+  public void validateDeliveryVerification(String trackingId) {
+    Response r = validateDeliveryVerificationAndGetRawResponse(trackingId);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+  }
+
+  public void editDeliveryVerificationRequired(String trackingId, String value) {
+    Response r = editDeliveryVerificationRequiredAndGetRawResponse(trackingId, value);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
   }
 
   public RescheduleOrderResponse rescheduleOrder(long orderId, RescheduleOrderRequest payload) {
@@ -119,6 +254,223 @@ public class OrderClient extends SimpleApiClient {
         .body(toJsonSnakeCase(payload));
 
     return doPost("Core - Reschedule Order", spec, url);
+  }
+
+  public Response cancelOrderV1AndGetRawResponse(Long orderId, String reason) {
+
+    String url = "core/orders/{orderId}/cancel";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .pathParam("orderId", orderId)
+        .body(f("{\"comments\":\"%s\"}", reason));
+    return doPut("Core - Cancel Order V1", spec, url);
+  }
+
+  public Response cancelOrder(long orderId) {
+    return cancelOrderV1AndGetRawResponse(orderId, "cancelled for testing purposes");
+  }
+
+  public void cancelOrderV1(Long orderId, String reason) {
+    Response response = cancelOrderV1AndGetRawResponse(orderId, reason);
+    if (response.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + response.statusCode());
+    }
+    response.then().body(equalToIgnoringCase("{\"message\":\"Order cancelled\"}"));
+  }
+
+  public Response cancelOrderV3AndGetRawResponse(String trackingId) {
+
+    String url = "core/2.2/orders/{trackingId}";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .pathParam("trackingId", trackingId);
+    return doDelete("Core - Cancel Order V3", spec, url);
+  }
+
+  public void cancelOrderV3(String trackingId) {
+    Response response = cancelOrderV3AndGetRawResponse(trackingId);
+    if (response.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + response.statusCode());
+    }
+  }
+
+  public Response resumeOrderRawResponse(String trackingId) {
+    String url = "core/orders/resume";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .body(f("[{\"trackingId\":\"%s\"}]", trackingId));
+
+    return doPost("Core - Resume Order", spec, url);
+  }
+
+  public void resumeOrder(String trackingId) {
+    Response r = resumeOrderRawResponse(trackingId);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+  }
+
+  public void setReturnedToSender(RtsOrderRequest rtsOrder) {
+    Response r = setReturnedToSenderAndGetRawResponse(rtsOrder);
+    if (r.statusCode() != HttpConstants.RESPONSE_204_NO_CONTENT) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+  }
+
+  public Response setReturnedToSenderAndGetRawResponse(RtsOrderRequest rtsOrder) {
+    String url = "core/orders/{orderId}/rts";
+    String json = toJsonSnakeCase(rtsOrder);
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .header("Accept", ContentType.JSON)
+        .header("Content-Type", ContentType.JSON)
+        .pathParam("orderId", rtsOrder.getOrderId())
+        .body(json);
+
+    return doPut("Core - Set Returned to Sender", spec, url);
+  }
+
+  public void revertRts(RtsOrderRequest rtsOrderDetail, Long orderId) {
+    String url = "/core/orders/{orderId}/revert-rts";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .header("Accept", ContentType.JSON)
+        .header("Content-Type", ContentType.JSON)
+        .pathParam("orderId", orderId)
+        .body(toJsonSnakeCase(rtsOrderDetail));
+
+    Response r = doPut("Core - Revert RTS Order", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_204_NO_CONTENT) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+  }
+
+  public void addOrderTo3pl(String trackingId, String thirdPartyTrackingId,
+      long thirdPartyShipperId) {
+    String url = "core/thirdpartyorders";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .header("Accept", ContentType.JSON)
+        .header("Content-Type", ContentType.JSON)
+        .body(
+            f("[{\"trackingId\":\"%s\",\"thirdPartyTrackingId\":\"%s\",\"thirdPartyShipperId\":%d}]",
+                trackingId, thirdPartyTrackingId, thirdPartyShipperId));
+
+    Response r = doPost("Core - Add order to 3PL", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+  }
+
+  public void addOrderLevelTags(long orderId, List<Long> tagIds) {
+    String url = "core/orders/tags/append-bulk";
+    OrderTagsRequest tag = new OrderTagsRequest();
+    tag.setOrderId(orderId);
+    tag.setTags(tagIds);
+    List<OrderTagsRequest> request = Collections.singletonList(tag);
+    String json = toJsonSnakeCase(request);
+    RequestSpecification spec = createAuthenticatedRequest()
+        .pathParam("orderId", orderId)
+        .body(json);
+
+    Response r = doPost("Core - Add Order Level Tags", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+  }
+
+  public List<String> getOrderLevelTags(long orderId) {
+    String url = "core/orders/{orderId}/tags";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .pathParam("orderId", orderId);
+
+    Response r = doGet("Core - Get Order Level Tags", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+
+    return fromJsonCamelCaseToList(r.getBody().asString(), String.class);
+  }
+
+  public List<SearchOrderTagResponse> searchOrderTag(List<Long> orderIds) {
+    String url = "core/orders/tags/search";
+    String json = "{\"order_ids\":" + toJsonSnakeCase(orderIds) + "}";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .body(json);
+
+    Response r = doPost("Core - Search Order Tags", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+
+    return fromJsonCamelCaseToList(r.getBody().asString(), SearchOrderTagResponse.class);
+  }
+
+  public List<Tag> getAllTags() {
+    String url = "core/orders/tags/all";
+
+    RequestSpecification spec = createAuthenticatedRequest();
+
+    Response r = doGet("Core - Get ALL Order Tags", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+
+    return r.body().jsonPath().getList(".", Tag.class);
+  }
+
+  public void deleteOrderTag(Long tagId) {
+    String url = "core/orders/tags/{tagId}";
+
+    RequestSpecification spec = createAuthenticatedRequest()
+        .pathParam("tagId", tagId);
+
+    Response r = doDelete("Core - Delete Order Tag", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+  }
+
+  public void deleteTagFromOrder(Map<String, Object> payLoad) {
+    String url = f("core/orders/%d/tags", payLoad.get("order_id"));
+    RequestSpecification spec = createAuthenticatedRequest().body(toJson(payLoad));
+    Response r = doDelete("Core - Delete Tag from the order", spec, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+  }
+
+  public void deleteOrderTag(String name) {
+    List<Tag> tags = getAllTags();
+    Tag tag = tags.stream()
+        .filter(t -> StringUtils.equalsIgnoreCase(t.getName(), name))
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("Tag [" + name + "] was not found"));
+    deleteOrderTag(tag.getId());
+  }
+
+  public Tag createOrderTag(Tag tag) {
+    String apiMethod = "core/orders/tags/new";
+
+    RequestSpecification requestSpecification = createAuthenticatedRequest()
+        .body(tag);
+
+    Response r = doPost("Order Client - Create Order Tag", requestSpecification, apiMethod);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+    return r.body().as(Tag.class);
   }
 
   private Order convertJsonOrder(String jsonOrder) {
@@ -189,5 +541,33 @@ public class OrderClient extends SimpleApiClient {
     } catch (Exception ex) {
       throw new RuntimeException(ex);
     }
+  }
+
+  public boolean waitUntilOrderState(Order expected, int timeoutInSeconds, int pauseInMillis) {
+    if (expected == null) {
+      throw new IllegalArgumentException("Expected order state must not be null");
+    }
+    if (StringUtils.isBlank(expected.getTrackingId())) {
+      throw new IllegalArgumentException("Tracking ID of an order was not specified");
+    }
+    long start = new Date().getTime();
+    boolean match = false;
+    String message = "Order " + expected.getTrackingId() + " has unexpected state";
+    int timeout = timeoutInSeconds * 1000;
+    do {
+      try {
+        Order actual = searchOrderByTrackingId(expected.getTrackingId());
+        expected.compareWithActual(actual);
+        match = true;
+      } catch (Throwable ex) {
+        LOGGER.debug(message);
+        try {
+          Thread.sleep(pauseInMillis);
+        } catch (InterruptedException e) {
+          LOGGER.error(e.getMessage(), e);
+        }
+      }
+    } while (!match && new Date().getTime() - start < timeout);
+    return match;
   }
 }
---
+++ src/main/java/co/nvqa/common/core/model/order/RtsOrderRequest.java
@@ -0,0 +1,49 @@
+package co.nvqa.common.core.model.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RtsOrderRequest {
+
+  private String address1;
+  private String address2;
+  private String city;
+  private String contact;
+  private String country;
+  private String date;
+  private String email;
+  private String name;
+  private Long orderId;
+  private String postcode;
+  private String reason;
+  private Long timewindowId;
+  private Double latitude;
+  private Double longitude;
+
+  public RtsOrderRequest(String address1, String address2, String city, String contact,
+      String country,
+      String date, String email, String name, Long orderId, String postcode, String reason,
+      Long timewindowId) {
+    this.address1 = address1;
+    this.address2 = address2;
+    this.city = city;
+    this.contact = contact;
+    this.country = country;
+    this.date = date;
+    this.email = email;
+    this.name = name;
+    this.orderId = orderId;
+    this.postcode = postcode;
+    this.reason = reason;
+    this.timewindowId = timewindowId;
+  }
+
+}
