1912240c43e435234aef9008847f763b95b62cd9
Binti Cahayati <binti.cahayati@ninjavan.co>
2023-03-01 10:49:15 +0700

    migrate shipperpickupclient

--- src/main/java/co/nvqa/common/core/client/PickupClient.java
+++ src/main/java/co/nvqa/common/core/client/PickupClient.java
@@ -7,6 +7,7 @@ import co.nvqa.common.core.model.pickup.PickupSearchRequest;
 import co.nvqa.common.utils.NvTestHttpException;
 import co.nvqa.common.utils.StandardTestConstants;
 import co.nvqa.commonauth.utils.TokenUtils;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import java.util.Collections;
@@ -21,7 +22,7 @@ public class PickupClient extends SimpleApiClient {
         DEFAULT_SNAKE_CASE_MAPPER);
   }
 
-  public List<Pickup> getPickups(long id) {
+  public List<Pickup> getPickupById(long id) {
     final PickupSearchRequest request = new PickupSearchRequest();
     request.setReservationIds(Collections.singletonList(id));
 
@@ -33,6 +34,14 @@ public class PickupClient extends SimpleApiClient {
     return fromJsonSnakeCaseToList(response.getBody().asString(), Pickup.class);
   }
 
+  public List<Pickup> searchPickupsWithFilters(PickupSearchRequest pickupFilter) {
+    Response r = getPickupsRawResponse(pickupFilter);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    return fromJsonSnakeCaseToList(r.body().asString(), Pickup.class);
+  }
+
   private Response getPickupsRawResponse(PickupSearchRequest request) {
     String url = "core/pickups/search";
     String json = toJsonSnakeCase(request);
@@ -41,5 +50,20 @@ public class PickupClient extends SimpleApiClient {
         .body(json);
 
     return doPost("Search Pickup", spec, url);
+  }
+
+  public Pickup getPickupWithPod(long reservationId) {
+    String url = "core/pickups/{reservationId}/pods";
+
+    RequestSpecification requestSpecification = createAuthenticatedRequest()
+        .pathParam("reservationId", reservationId);
+
+    Response r = doGet(f("Core - Get Pickup for Reservation with ID = %d", reservationId),
+        requestSpecification, url);
+    if (r.statusCode() != HttpConstants.RESPONSE_200_SUCCESS) {
+      throw new NvTestHttpException("unexpected http status: " + r.statusCode());
+    }
+    r.then().contentType(ContentType.JSON);
+    return fromJson(r.body().asString(), Pickup.class);
   }
 }
