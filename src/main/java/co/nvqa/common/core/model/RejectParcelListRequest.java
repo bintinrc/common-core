69b30466abefc7513b6ad3010b1a1359e47491c3
Anderson <anderson.susanto@ninjavan.co>
2023-03-06 09:58:12 +0700

    add Lombok

--- src/main/java/co/nvqa/common/core/model/RejectParcelListRequest.java
+++ src/main/java/co/nvqa/common/core/model/RejectParcelListRequest.java
@@ -1,10 +1,14 @@
 package co.nvqa.common.core.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RejectParcelListRequest {
 
   private String trackingId;
