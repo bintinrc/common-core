16599072c9ee7c5cd20dd3b1b36ea26353987788
Diaz Ilyasa <diaz.ilyasa@ninjavan.co>
2023-05-05 13:17:09 +0700

    add new scenario for fetch reservations

--- src/main/java/co/nvqa/common/core/cucumber/glue/DbCoreSteps.java
+++ src/main/java/co/nvqa/common/core/cucumber/glue/DbCoreSteps.java
@@ -64,6 +64,16 @@ public class DbCoreSteps extends CoreStandardSteps {
   public void init() {
   }
 
+  @And("DB Core - get Reservation data from reservation id {string}")
+  public void coreGetReservationDataFromReservationId(String reservationId) {
+    Long resolvedReservationIdKey = Long.parseLong(resolveValue(reservationId));
+    retryIfAssertionErrorOrRuntimeExceptionOccurred(() -> {
+      Reservations result = reservationDao.getReservationsDetailsByReservationId(
+          resolvedReservationIdKey);
+      putInList(KEY_LIST_OF_RESERVATIONS_DB, result);
+    }, "Fetch ReservationData", 2000, 3);
+  }
+
   @And("DB Core - get waypoint Id from reservation id {string}")
   public void coreGetWaypointFromReservationId(String reservationId) {
     Long resolvedReservationIdKey = Long.parseLong(resolveValue(reservationId));
