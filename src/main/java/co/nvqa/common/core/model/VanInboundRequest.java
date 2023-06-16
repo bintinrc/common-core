41f3d6f7746a03a818ce2b5cc0b13e44d9bb2ea2
Aaron Purnomo Murniadi <aaron.murniadi@ninjavan.co>
2023-06-16 17:51:47 +0700

    use Lombok annotation for VanInboundRequest model

--- src/main/java/co/nvqa/common/core/model/VanInboundRequest.java
+++ src/main/java/co/nvqa/common/core/model/VanInboundRequest.java
@@ -1,32 +1,18 @@
 package co.nvqa.common.core.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VanInboundRequest {
 
   private String trackingId;
   private Long waypointId;
   private String type;
 
-  public String getTrackingId() {
-    return trackingId;
-  }
-
-  public void setTrackingId(String trackingId) {
-    this.trackingId = trackingId;
-  }
-
-  public Long getWaypointId() {
-    return waypointId;
-  }
-
-  public void setWaypointId(Long waypointId) {
-    this.waypointId = waypointId;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
 }
