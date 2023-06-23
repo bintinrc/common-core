025e20ef85e977c61c9e5468843bd077116aa9d2
Diaz Ilyasa <diaz.ilyasa@ninjavan.co>
2023-06-23 14:44:20 +0700

    refactor few bean class for adding setter getter

--- src/main/java/co/nvqa/common/core/model/edit_delivery_order/RecipientDetail.java
+++ src/main/java/co/nvqa/common/core/model/edit_delivery_order/RecipientDetail.java
@@ -1,7 +1,17 @@
 package co.nvqa.common.core.model.edit_delivery_order;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RecipientDetail {
   private String name;
   private String email;
