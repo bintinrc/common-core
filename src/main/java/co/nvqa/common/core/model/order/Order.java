fdcea4f9de0aa0080c2d9f920a4583d8cf774afc
Rizaq Pratama <rizaq.pratama@ninjavan.co>
2023-12-22 14:07:15 +0700

    order search client

--- src/main/java/co/nvqa/common/core/model/order/Order.java
+++ src/main/java/co/nvqa/common/core/model/order/Order.java
@@ -4,6 +4,7 @@ import co.nvqa.common.model.DataEntity;
 import co.nvqa.common.utils.JsonUtils;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -41,6 +42,7 @@ import static org.apache.commons.lang3.StringUtils.normalizeSpace;
 @SuppressWarnings("unused")
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Order extends DataEntity<Order> implements Serializable {
 
   public static final String STATUS_TRANSIT = "TRANSIT";
