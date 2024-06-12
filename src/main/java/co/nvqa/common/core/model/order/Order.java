e00671e927502d0729e22eddf29febb411584fed
Abdullah SaadEddin <abdullah.saadeddin@ninjavan.co>
2024-06-12 09:08:59 +0300

    change float to double

--- src/main/java/co/nvqa/common/core/model/order/Order.java
+++ src/main/java/co/nvqa/common/core/model/order/Order.java
@@ -781,7 +781,7 @@ public class Order extends DataEntity<Order> implements Serializable {
     private String itemDescription;
     private Boolean isDangerousGood;
     private Integer quantity;
-    private Float unitValue;
+    private Double unitValue;
   }
 
   @Getter
